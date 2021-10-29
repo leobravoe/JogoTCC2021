57a0a4d6c8b5b8da162b19d039c7b568fc0346bd
leobravoe <leonardo.bravo.estacio@gmail.com>
2021-10-28 22:37:47 -0300

    Commit dia 28-10-2021

--- Assets/Scripts/MoveToNext.cs
+++ Assets/Scripts/MoveToNext.cs
@@ -14,13 +14,28 @@ public class MoveToNext : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         // Se existir um NavMeshAgent dentro do objeto que colidiu
-        if(other.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>())
+        if (other.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>())
         {
             // Pego a referência do NavMeshAgent dentro do objeto que colidiu e armazeno em "nav"
             UnityEngine.AI.NavMeshAgent nav = other.gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
             // Seto o destino como a posição do próximo objeto
-            nav.destination = next.transform.position;
-            print("Destino:" + nav.destination);
+            nav.destination = NextChild().position;
         }
+    }
+
+    // Update is called once per frame
+    private Transform NextChild()
+    {
+        // Check where we are
+        int thisIndex = this.transform.GetSiblingIndex();
+
+        // We have a few cases to rule out
+        if (this.transform.parent == null)
+            return null;
+        if (this.transform.parent.childCount <= thisIndex + 1)
+            return this.transform.parent.GetChild(0);
+
+        // Then return whatever was next, now that we're sure it's there
+        return this.transform.parent.GetChild(thisIndex + 1);
     }
 }
--- Assets/Scripts/PlayerNavgator.cs
+++ Assets/Scripts/PlayerNavgator.cs
@@ -6,6 +6,15 @@ using UnityEngine.AI;
 public class PlayerNavgator : MonoBehaviour
 {
     NavMeshAgent nav;
+    public Transform backLeft;
+    public Transform backRight;
+    public Transform frontLeft;
+    public Transform frontRight;
+    private RaycastHit lr;
+    private RaycastHit rr;
+    private RaycastHit lf;
+    private RaycastHit rf;
+    private Vector3 upDir;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +26,28 @@ public class PlayerNavgator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Tenta ajustar a inclinação do modelo
+        Physics.Raycast(backLeft.position + Vector3.up, Vector3.down, out lr);
+        Physics.Raycast(backRight.position + Vector3.up, Vector3.down, out rr);
+        Physics.Raycast(frontLeft.position + Vector3.up, Vector3.down, out lf);
+        Physics.Raycast(frontRight.position + Vector3.up, Vector3.down, out rf);
+
+        upDir = (Vector3.Cross(rr.point - Vector3.up, lr.point - Vector3.up) +
+                 Vector3.Cross(lr.point - Vector3.up, lf.point - Vector3.up) +
+                 Vector3.Cross(lf.point - Vector3.up, rf.point - Vector3.up) +
+                 Vector3.Cross(rf.point - Vector3.up, rr.point - Vector3.up)
+                ).normalized;
+        Debug.DrawRay(rr.point, Vector3.up);
+        Debug.DrawRay(lr.point, Vector3.up);
+        Debug.DrawRay(lf.point, Vector3.up);
+        Debug.DrawRay(rf.point, Vector3.up);
+        Debug.DrawRay(transform.position, upDir);
+
+        transform.up = upDir;
+
+        print(upDir);
+
+
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;

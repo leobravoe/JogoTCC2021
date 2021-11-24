9c95adff1f38295a625cf0ad0e87a2f2d4c4bf00
joaopbaz <jpbaz0069@gmail.com>
2021-11-24 16:16:02 -0300

    Menu Funcional

---
+++ Assets/_ProjectFiles/Scripts/Menu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void iniciar()
+    {
+        SceneManager.LoadScene("Interlagos");
+    }
+
+    public void sair()
+    {
+        //No Edtior
+        UnityEditor.EditorApplication.isPlaying = false;
+
+        //No Jogo
+        // Application.Quit();
+    }
+}
